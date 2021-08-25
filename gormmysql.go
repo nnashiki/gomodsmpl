5d2142719cd49427bd9e19d4e49c424ed0ce935e
nashiki <n.nashiki.work@gmail.com>
2021-08-25 23:15:17 +0900

    update: データの保存ができるようになった

--- gormmysql.go
+++ gormmysql.go
@@ -11,6 +11,18 @@ type Product struct {
 	Price uint
 }
 
+type User struct {
+	gorm.Model
+	Name      string
+	CompanyID int
+	Company   Company
+}
+
+type Company struct {
+	ID   int
+	Name string
+}
+
 func main() {
 	// 詳細は https://github.com/go-sql-driver/mysql#dsn-data-source-name を参照
 	dsn := "baseball_user:baseball_pass@tcp(127.0.0.1:3306)/baseball_db?charset=utf8mb4&parseTime=True&loc=Local"
@@ -18,9 +30,12 @@ func main() {
 
 	// Migrate the schema
 	db.AutoMigrate(&Product{})
+	db.AutoMigrate(&User{})
+	db.AutoMigrate(&Company{})
 
 	// Create
 	db.Create(&Product{Code: "D42", Price: 100})
+	db.Create(&User{Name: "nashiki", Company: Company{Name: "サイバーダイン"}})
 
 	// Read
 	var product Product
